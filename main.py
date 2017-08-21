9a800243831ce57812dd849779700d741d0b64b2
Celien Nanson <c.nanson@sofort.com>
2017-08-21 20:08:16 +0200

    Finally load VGG model into Tensorflow and extract Tensors from it

--- main.py
+++ main.py
@@ -24,8 +24,6 @@ def load_vgg(sess, vgg_path):
 	:param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
 	:return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
 	"""
-	# TODO: Implement function
-	#   Use tf.saved_model.loader.load to load the model and weights
 	vgg_tag = 'vgg16'
 	vgg_input_tensor_name = 'image_input:0'
 	vgg_keep_prob_tensor_name = 'keep_prob:0'
@@ -34,7 +32,8 @@ def load_vgg(sess, vgg_path):
 	vgg_layer7_out_tensor_name = 'layer7_out:0'
 
 	with sess.as_default(): 
-		graph = tf.saved_model.loader.load(sess, ['vgg16'], vgg_path)
+		tf.saved_model.loader.load(sess, ['vgg16'], vgg_path)
+		graph = sess.graph
 		image_input = graph.get_tensor_by_name(vgg_input_tensor_name)
 		keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
 		layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
